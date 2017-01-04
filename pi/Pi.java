eab3a178166fdbd745476ed018752cf9843e04b3
Anuv Gupta <me@anuv.me>
2017-01-04 15:10:13 -0800

    remove gpdraw

--- pi/Pi.java
+++ pi/Pi.java
@@ -1,22 +1,11 @@
-import gpdraw.*;
-import java.awt.Color;
 
 public class Pi {
     public static void main(String[] args) {
         double pi = 0;
 
-        /*
-        DrawingTool pen = new DrawingTool(new SketchPad(500, 500));
-        pen.up();
-        pen.move(0, 0);
-        pen.down();
-        pen.drawCircle(100);
-        pen.up();
-        pen.setColor(Color.WHITE);
-         */
-
-        double r = 20;
+        // find length of circumference
         double c = 0;
+        double r = 20;
         double n = 10e+7;
         for (double i = 1; i < n; i++) {
             double xi0 = 2 * r * i / n - r;
@@ -25,17 +14,11 @@ public class Pi {
             double fxi1 = Math.sqrt(r * r - xi1 * xi1);
             c += Math.sqrt(Math.pow(xi1 - xi0, 2) + Math.pow(fxi1 - fxi0, 2));
             System.out.printf("(%.5f, %.5f) | (%.5f, %.5f) %n", xi0, fxi0, xi1, fxi1);
-            
-            /*
-            pen.move(xi0, fxi0);
-            pen.down();
-            pen.move(xi1, fxi1);
-            pen.up();
-             */
         }
         c *= 2;
+        
+        // find pi from circumference
         pi = c / (2 * r);
-
         System.out.printf("Calc Pi: %.20f%n", pi);
         System.out.printf("Math.PI: %.20f%n", Math.PI);
     }
